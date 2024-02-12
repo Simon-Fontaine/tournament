e39cec02109d0257ba1451b9333a5879971c6dcc
Simon Fontaine <74004726+Simon-Fontaine@users.noreply.github.com>
2024-02-12 11:56:48 +0100

    default application template

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import '@/styles/globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Tournament',
+    default: 'Tournament',
+  },
+  description: 'Tournament Wep App, Create, Manage or Join online tournaments',
+  metadataBase: new URL('https://github.com/Simon-Fontaine/tournament'),
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col p-6">
+      <div>
+        <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+          similique ut libero velit magnam amet a impedit in voluptas? Esse
+          facere facilis animi aliquid unde odio delectus explicabo, distinctio
+          optio!
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga
+          mollitia aliquid minima neque expedita, totam dolorum eaque odit
+          quibusdam omnis nemo magni ducimus incidunt animi quidem ex. Maiores,
+          ipsa tempore.
+        </p>
+      </div>
+    </main>
+  );
+}
